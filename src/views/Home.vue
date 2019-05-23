da5c843b065099a0cb75d15a86252cb5814b1e89
goodude123 <assasinmcpl@gmail.com>
2019-05-23 11:38:08 +0200

    Home View.

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h2>This is a home page</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+};
+</script>
+
+<style lang="scss" scoped>
+
+.sign-button {
+  width: 10%;
+  margin: 10px;
+  background-color: #00FA9A;
+  font-weight: bold;
+}
+
+.sign-button:hover {
+  background-color: #00FF7F;
+}
+</style>
