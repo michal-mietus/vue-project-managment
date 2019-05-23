0f678fbb435a7e14b8c2077e9960a601b2b5cc6b
goodude123 <assasinmcpl@gmail.com>
2019-05-23 11:37:33 +0200

    Authentication views.

---
+++ src/views/authentication/LoginView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="login">
+    <h2>Login</h2>
+    <form>
+      <input type="text" v-model="username" placeholder="username" required>
+      <input type="password" v-model="password" placeholder="password" required>
+      <input type="submit" value="Login" @click="login">
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    login: function () {
+      let username = this.username;
+      let password = this.password;
+      this.$store.dispatch('login', { username, password })
+        .then(() => this.$router.push('/'))
+        .catch(error => console.error(error))
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.sign-button {
+  width: 10%;
+  margin: 10px;
+  background-color: #00FA9A;
+  font-weight: bold;
+}
+
+.sign-button:hover {
+  background-color: #00FF7F;
+}
+</style>
---
+++ src/views/authentication/RegisterView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="register">
+    <h2>Register</h2>
+    <form @submit="register()">
+      <input type="text" v-model="username" placeholder="Username" required>
+      <input type="email" v-model="email" placeholder="Email" required>
+      <input type="password" v-model="password" placeholder="Password" required>
+      <input type="password" v-model="passwordConfirm" placeholder="Password Confirm" required>
+      <input type="submit" value="Register">
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'register',
+
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    register: function () {
+      let data = {
+        username: this.username,
+        email: this.email,
+        password: this.password
+      }
+
+      this.$store.dispatch('register', data)
+       .then(() => this.$router.push('/'))
+       .catch(error => console.error(error))
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.sign-button {
+  width: 10%;
+  margin: 10px;
+  background-color: #00FA9A;
+  font-weight: bold;
+}
+
+.sign-button:hover {
+  background-color: #00FF7F;
+}
+</style>
