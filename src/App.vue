140f2570f276b7cd2df181b77f789e3b7e34e7ca
goodude123 <assasinmcpl@gmail.com>
2019-05-23 11:38:14 +0200

    App.

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <Navigation/>
+  </div>
+</template>
+
+<script>
+import Navigation from './components/base/Navigation.vue';
+
+export default {
+  components: {
+    Navigation,
+  },
+
+  created: function () {
+    this.$http.interceptors.response.use(undefined, function (err) {
+      return new Promise(function (resolve, reject) {
+        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+          this.$store.dispatch(logout)
+        }
+        throw err;
+      });
+    });
+  }
+};
+
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
